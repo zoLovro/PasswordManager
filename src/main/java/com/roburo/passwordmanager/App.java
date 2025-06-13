a703ed013e4d847b564ce9a0c7b676d49117c1f4
zoLovro <lovrohvalakopar@gmail.com>
2025-06-13 21:14:30 +0200

    Build

---
+++ src/main/java/com/roburo/passwordmanager/App.java
@@ -0,0 +1,7 @@
+package com.roburo.passwordmanager;
+
+public class App {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.roburo.passwordmanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.roburo.passwordmanager to javafx.fxml;
-    exports com.roburo.passwordmanager;
-}
